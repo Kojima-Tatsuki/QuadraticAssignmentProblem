4841c142eb4afd1febc84117ec13e596e42bf601
Sona <51908834+Kojima-Tatsuki@users.noreply.github.com>
2023-10-04 15:29:42 +0900

    インターフェースの追加

---
+++ Model/Search/ISearch.cs
@@ -0,0 +1,25 @@
+﻿namespace QAP.Model.Search
+{
+    internal interface ISearch
+    {
+        public ProblemModel Problem { get; init; }
+
+        public SearchResult Search(IReadOnlyList<int> initAns);
+    }
+
+    internal record SearchResult
+    {
+        public IReadOnlyList<int> InitAns { get; init; }
+        public IReadOnlyList<int> Ans { get; init; }
+        public int Score { get; init; }
+        public ProblemModel Problem { get; init; }
+
+        public SearchResult(IReadOnlyList<int> initAns, IReadOnlyList<int> ans, int score, ProblemModel problem)
+        {
+            InitAns = initAns;
+            Ans = ans;
+            Score = score;
+            Problem = problem;
+        }
+    }
+}
---
+++ Model/Search/LocalSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAP.Model.Search
+{
+    internal class LocalSearch : ISearch
+    {
+        public ProblemModel Problem { get; init; }
+
+        public LocalSearch(ProblemModel problem)
+        {
+            Problem = problem;
+        }
+
+        public SearchResult Search(IReadOnlyList<int> initAns)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
