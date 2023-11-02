9b5c6706d9794e1792c327de592d0954336beaa3
Sona <51908834+Kojima-Tatsuki@users.noreply.github.com>
2023-11-03 02:40:36 +0900

    経過時間と経過率の表示を追加

---
+++ CharacterUserInterface/PercentageShower.cs
@@ -0,0 +1,20 @@
+﻿namespace QAP.CharacterUserInterface
+{
+    internal class PercentageShower
+    {
+        public int MaxCount { get; init; }
+        public DateTime StartTime { get; init; }
+
+        public PercentageShower(int maxCount)
+        {
+            MaxCount = maxCount;
+            StartTime = DateTime.Now;
+        }
+
+        public void UpdateConsole(int count)
+        {
+            var secondSpan = (DateTime.Now - StartTime).Seconds;
+            Console.WriteLine($"{count} / {MaxCount} ({MathF.Round((float)count / MaxCount * 100, 2)}%), {secondSpan} seconds elapsed.");
+        }
+    }
+}
